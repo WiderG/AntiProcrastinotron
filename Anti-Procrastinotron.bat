863c2dfd0c3a092c758d61d1a724be1e64b435e3
WiderG <142853127+WiderG@users.noreply.github.com>
2023-10-15 19:42:32 +0100

    Create Anti-Procrastinotron.bat

---
+++ Anti-Procrastinotron.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Nathan, CLEAN YOUR FUCKING ROOM (Locking for 15 minutes)
+timeout /t 1 >nul
+rundll32.exe user32.dll,LockWorkStation
+timeout /t 900 >nul
+echo Welcome back! Your PC is now unlocked.
